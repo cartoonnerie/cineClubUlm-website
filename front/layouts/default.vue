0387b2fa15e30a9b32b7ad569f55288154f3dd99
cartoonnerie <melodia@melix.org>
2021-10-31 00:08:28 +0200

    init front

---
+++ front/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="layout">
+  <Header />
+  <div class="container mx-auto">
+    <Nuxt />
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
---
+++ front/tailwind.config.js
@@ -0,0 +1,23 @@
+/*
+** TailwindCSS Configuration File
+**
+** Docs: https://tailwindcss.com/docs/configuration
+** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+*/
+module.exports = {
+  theme: {
+    container: {
+      center: true
+    }
+  }
+  // purge: {
+  //   enabled: process.env.NODE_ENV === 'production',
+  //   content: [
+  //     'components/**/*.vue',
+  //     'layouts/**/*.vue',
+  //     'pages/**/*.vue',
+  //     'nuxt.config.js'
+  //   ]
+  // },
+  // darkMode: false
+}
